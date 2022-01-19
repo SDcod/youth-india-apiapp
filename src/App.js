f1270bf200d334ec3233b93195649b03a978913a
SDcod <67920903+SDcod@users.noreply.github.com>
2022-01-19 16:36:46 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [visible, setVisible] = useState(4);
+  useEffect(() => {
+    axios.get("https://api.disneyapi.dev/characters").then((res) => {
+      setData(res.data.data);
+    });
+  }, []);
+  const loadmoreHandler = () => {
+    setVisible((prevValue) => prevValue + 4);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 className='heading'>DISNEY CHARACTERS</h1>
+      <input
+        className='search_bar'
+        type='text'
+        placeholder='Search...'
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
+      />
+      <div className='mainpage'>
+        {data && data.length > 0 ? (
+          data
+            .filter((person) => {
+              if (searchTerm === "") {
+                return person;
+              } else if (
+                person.name
+                  .toString()
+                  .toLowerCase()
+                  .includes(searchTerm.toString().toLowerCase())
+              ) {
+                return person;
+              }
+            })
+            .slice(0, visible)
+            .map((char) => {
+              return (
+                <div className='main_card' key={char._id}>
+                  <img
+                    className='image'
+                    src={char.imageUrl}
+                    alt='no image available
+            '
+                  />
+                  <div className='desc_container'>
+                    <div className='name'>
+                      Name: <p> {char.name}</p>
+                    </div>
+
+                    <div className='shows'>
+                      <p>-: Shows :-</p>
+                      <p className='film_name'> {char.films}</p>
+                      <p className='tv_shows'> {char.tvShows}</p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })
+        ) : (
+          <div className='loader'></div>
+        )}
+      </div>
+      <button onClick={loadmoreHandler} className='loadmore'>
+        load more...
+      </button>
     </div>
   );
 }
